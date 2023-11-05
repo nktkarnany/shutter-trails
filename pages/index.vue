7059eabd17d4bceec926560b92c962dcab887aa6
Ankit Karnany <ankitkarnany@gmail.com>
2023-11-05 12:21:44 +0100

    chore: grid lines drawing

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="home-page">
-    <st-photo />
+  <div class="home-page grid">
+    <div v-for="i in 24 * 15" :key="i"></div>
+    <!-- <st-photo /> -->
   </div>
 </template>
 
@@ -10,5 +11,31 @@
 .home-page {
   width: 100%;
   height: 100%;
+  // display: grid;
+  // grid-template-columns: repeat(24, 1fr);
+  // grid-template-rows: repeat(15, 1fr);
+}
+
+.grid {
+  display: grid;
+  grid-template-columns: repeat(24, 1fr);
+  grid-template-rows: repeat(15, 1fr);
+}
+
+// .grid::before {
+//   content: '';
+//   width: 0;
+//   padding-bottom: 100%;
+//   grid-row: 1 / 1;
+//   grid-column: 1 / 1;
+// }
+
+// .grid > *:first-child {
+//   grid-row: 1 / 1;
+//   grid-column: 1 / 1;
+// }
+
+.grid > * {
+  border: 1px solid var(--gray-700);
 }
 </style>
