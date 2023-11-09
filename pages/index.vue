0c3713696f4a5c93e0c8b185324d7a0c4b5d05a5
Ankit Karnany <ankitkarnany@gmail.com>
2023-11-09 21:56:57 +0100

    chore: created grids and added photo

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="home-page grid">
-    <div v-for="i in 24 * 15" :key="i"></div>
-    <!-- <st-photo /> -->
+  <div class="home-page">
+    <div class="grid">
+      <div class="grid-box" v-for="i in 24 * 15" :key="i"></div>
+    </div>
+    <div class="grid">
+      <st-photo />
+    </div>
   </div>
 </template>
 
@@ -11,31 +15,25 @@
 .home-page {
   width: 100%;
   height: 100%;
-  // display: grid;
-  // grid-template-columns: repeat(24, 1fr);
-  // grid-template-rows: repeat(15, 1fr);
+  position: relative;
 }
 
 .grid {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
   display: grid;
   grid-template-columns: repeat(24, 1fr);
   grid-template-rows: repeat(15, 1fr);
-}
 
-// .grid::before {
-//   content: '';
-//   width: 0;
-//   padding-bottom: 100%;
-//   grid-row: 1 / 1;
-//   grid-column: 1 / 1;
-// }
-
-// .grid > *:first-child {
-//   grid-row: 1 / 1;
-//   grid-column: 1 / 1;
-// }
-
-.grid > * {
-  border: 1px solid var(--gray-700);
+  &-box {
+    margin: 2px;
+    border-width: 1px;
+    border-style: solid;
+    border-color: var(--gray-700);
+    opacity: 0.1;
+  }
 }
 </style>
